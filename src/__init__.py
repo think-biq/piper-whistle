2867f8dbca7ba9637511b8416731608b4e7caee0
blurryroots <blurryroots@posteo.de>
2023-11-23 16:41:17 +0100

    source setup and doxygen generator

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""    
+    CLI tool to manage piper voices.
+
+    2023-∞ (c) blurryroots innovation qanat OÜ. All rights reserved.
+    See license.md for details.
+
+    https://think-biq.com
+"""
