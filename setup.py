78f2a83ecef195a4f0617bbbccb113024f67831d
blurryroots <blurryroots@posteo.de>
2023-11-24 01:21:37 +0100

    package requirements and setup script

---
+++ setup.py
@@ -0,0 +1,62 @@
+"""    
+	setuptools script to install / build LLV.
+
+	2021-∞ (c) blurryroots innovation qanat OÜ. All rights reserved.
+	See license.md for details.
+
+	https://think-biq.com
+"""
+
+import setuptools
+from src.piper_whistle import version
+
+with open("readme.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup (
+	name = "piper_whistle",
+	version = version (),
+	author = "biq",
+	author_email = "sf@think-biq.com",
+	description = "CLI tool to manage piper voices.",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://gitlab.com/think-biq/piper-whistle",
+	package_dir = { 'piper_whistle': 'src/piper_whistle' },
+	packages = ['piper_whistle'],
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires = '>=3.8',
+	entry_points = {
+		'console_scripts': [
+			'piper_whistle = piper_whistle.cli:main'
+		],
+	},
+	install_requires=[
+		'attrs==23.1.0',
+		'certifi==2023.7.22',
+		'charset-normalizer==3.3.2',
+		'filelock==3.13.1',
+		'fsspec==2023.10.0',
+		'huggingface-hub==0.18.0',
+		'idna==3.4',
+		'markdown-it-py==3.0.0',
+		'mdurl==0.1.2',
+		'packaging==23.2',
+		'pkg_resources==0.0.0',
+		'Pygments==2.16.1',
+		'pypng==0.20220715.0',
+		'PyYAML==6.0.1',
+		'rapidfuzz==3.5.1',
+		'requests==2.31.0',
+		'rich==13.6.0',
+		'thefuzz==0.20.0',
+		'tqdm==4.66.1',
+		'typing_extensions==4.8.0',
+		'urllib3==2.0.7',
+		'userpaths==0.1.3',
+	]
+)
