51750a096dedbd18109be851c4b0660c4aeb2527
blurryroots <blurryroots@posteo.de>
2024-01-11 16:54:18 +0100

    adjusted tests
    added context aware (call-site) logging
    change test to reflect setup

--- src/testing/functional.py
+++ src/testing/functional.py
@@ -9,19 +9,26 @@ up a temporary directory, which is used as app data root for whistle. All
 
 """
 # 2023-∞ (c) blurryroots innovation qanat OÜ. All rights reserved.
-from ..piper_whistle import cli as whistle_cli
-from ..piper_whistle import db as whistle_db
-import unittest
-from unittest.mock import patch
 import sys
 import tempfile
 import os
 import pathlib
 import io
 import re
-from contextlib import redirect_stdout
+import inspect
+import logging
+import unittest
 from unittest import TestCase
+from unittest.mock import patch
+from contextlib import redirect_stdout
 from directory_tree import display_tree
+
+from ..piper_whistle import cli as whistle_cli
+from ..piper_whistle import db as whistle_db
+from ..piper_whistle import holz
+
+
+DEBUG = False
 
 
 expected_list_languages = """
@@ -104,11 +111,73 @@ class CliCommandTests (unittest.TestCase):
 	""" Using goofy naming scheme for test case ordering.
 	Suggested here https://stackoverflow.com/a/7085051
 	"""
+	LOG_SETUP_SILENT = True # Whether holz should be silent.
+	LOG = logging.getLogger('CliCommandTests') # Local (class) logger.
+
+	def _get_function_name_of_frame(self, frame_i = 1):
+		funcname = inspect.getframeinfo(sys._getframe(frame_i))[2]
+		return funcname
+
+	def _log_debug(self, msg):
+		funcname = self._get_function_name_of_frame(2)
+		category = self.LOCAL_LOG_DICT[funcname].name
+		holz.debug(msg, category = category, flush = True)
+
+	def _log_info(self, msg):
+		funcname = self._get_function_name_of_frame(2)
+		category = self.LOCAL_LOG_DICT[funcname].name
+		holz.info(msg, category = category, flush = True)
+
+	def _log_warn(self, msg):
+		funcname = self._get_function_name_of_frame(2)
+		category = self.LOCAL_LOG_DICT[funcname].name
+		holz.warn(msg, category = category, flush = True)
+
+	def _log_error(self, msg):
+		funcname = self._get_function_name_of_frame(2)
+		category = self.LOCAL_LOG_DICT[funcname].name
+		holz.error(msg, category = category, flush = True)
+
+	def _run_whistle_main (self, args):
+		cli_output = io.StringIO ()
+		with redirect_stdout (cli_output):
+			with patch.object (sys, 'argv', args):
+				whistle_cli.main (args)
+
+		out_str = cli_output.getvalue ().strip ()
+		return out_str
+
+	def _run_whistle_main_with (self, arg_list_name):
+		out_str = self._run_whistle_main (self.test_arguments[arg_list_name])
+		return out_str
 
 	@classmethod
 	def setUpClass (cls):
 		# TestCase wide setup (as opposed to individual test based setUp ())
-		sys.stdout.write (f'[Setup] Creating environemnt for "{cls}" ...\n')
+
+		cls.LOCAL_LOG_DICT = {}
+		# Fetch all methods which are supposed to be tests.
+		for funcname in dir (cls):
+			if funcname.startswith ('test_'):
+				cls.LOCAL_LOG_DICT[funcname] = logging.getLogger (
+					f'{cls.LOG.name}::{funcname}'
+				)
+
+		# Set appropriate log levels.
+		log_level = logging.INFO
+		if DEBUG:
+			log_level = logging.DEBUG
+
+		overrides = {}
+
+		# Setup and initialize holz logging utility.
+		holz.setup_default (cls.LOG.name
+			, log_level
+			, silent = cls.LOG_SETUP_SILENT
+		)
+		holz.normalize (overrides = overrides, silent = cls.LOG_SETUP_SILENT)
+
+		cls.LOG.debug (f'[Setup] Creating environemnt for "{cls}" ...')
 
 		# Create a temporary app data root directory
 		cls.tmp_dir = tempfile.TemporaryDirectory ()
@@ -131,28 +200,21 @@ class CliCommandTests (unittest.TestCase):
 			'remove-voice': make_cli_arg_list ('remove', 'de_DE-eva_k-x_low')
 		}
 
-		sys.stdout.write (f'[Setup] Temporary data root: "{data_root}"\n')
+		cls.LOG.info (f'[Setup] Temporary data root: "{data_root}"')
 
 	@classmethod
 	def tearDownClass (cls):
-		sys.stdout.write (f'\n[Teardown] Data directory after running tests:\n')
-		display_tree (cls.data_root_path.as_posix ())
-
-	def _run_whistle_main (self, args):
-		cli_output = io.StringIO ()
-		with redirect_stdout (cli_output):
-			with patch.object (sys, 'argv', args):
-				whistle_cli.main (args)
-
-		out_str = cli_output.getvalue ().strip ()
-		return out_str
-
-	def _run_whistle_main_with (self, arg_list_name):
-		out_str = self._run_whistle_main (self.test_arguments[arg_list_name])
-		return out_str
+		rp = cls.data_root_path.as_posix ()
+		print (f'\n[Teardown] Data directory "{rp}" after running tests:')
+		display_tree (rp)
 
 	def test_a0_create_index (self):
 		# Ask whistle to downlnoad and rebuild voice database.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('create-index')
 		self.assertTrue (check_if_timestamp (out_str))
 
@@ -161,12 +223,22 @@ class CliCommandTests (unittest.TestCase):
 
 	def test_b0_list_languages (self):
 		# Ask whistle to list all available languages.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('list-languages')
 
 		self.assertEqual (out_str, expected_list_languages.strip ())
 
 	def test_c0_list_voice (self):
 		# Ask whistle to list the first voice for the german language.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('list-voice')
 
 		self.assertEqual (out_str, expected_list_voice.strip ())
@@ -174,11 +246,21 @@ class CliCommandTests (unittest.TestCase):
 	def test_d0_list_voice_legal (self):
 		# Ask whistle to list the first voice for the german language and the
 		# available legal information.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('list-voice-legal')
 		self.assertEqual (out_str, expected_list_voice_plus_legal.strip ())
 
 	def test_e0_install_voice (self):
 		# Ask whistle to install the first german voice at index 0.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('install-voice')
 		out_parts = out_str.split ('\t')
 
@@ -194,19 +276,34 @@ class CliCommandTests (unittest.TestCase):
 
 	def test_f0_list_installed_voices (self):
 		# Ask whistle to list all installed voices.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('list-installed-voices')
 		self.assertEqual (out_str, expected_list_installed_voices.strip ())
 
 	def test_g0_show_voice_path (self):
 		# Ask whistle to show the path to the voice with name 'de_DE-eva_k-x_low'.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('show-path')
 		self.assertTrue (out_str.endswith (expected_show_path.strip ()))
 
 	def test_h0_remove_voice (self):
 		# Ask whistle to remove voice named 'de_DE-eva_k-x_low'.
+		funcname = self._get_function_name_of_frame(1)
+		_info = self._log_info
+
+		_info (f'Running {funcname} ...')
+
 		out_str = self._run_whistle_main_with ('remove-voice')
 		self.assertEqual (out_str, expected_remove_voice.strip ())
 
 
 if __name__ == '__main__':
-    unittest.main ()
+	unittest.main ()
